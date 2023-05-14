1e980f60dc999da4e5130e32861915c8b8e0d7dc
AnthonyGKruger <60028700+AnthonyGKruger@users.noreply.github.com>
2023-05-14 16:44:12 +0200

    initial commit

--- next.config.js
+++ next.config.js
@@ -5,16 +5,16 @@ const nextConfig = {
 
 
 
-module.exports = {
-  webpack: (config, options) =>
-  {
-      config.module.rules.push({
-          test: /\.pdf$/i,
-          type: 'asset/source'
-      })
+// module.exports = {
+//   webpack: (config, options) =>
+//   {
+//       config.module.rules.push({
+//           test: /\.pdf$/i,
+//           type: 'asset/source'
+//       })
 
-      return config
-  },
-}
+//       return config
+//   },
+// }
 
 module.exports = nextConfig
