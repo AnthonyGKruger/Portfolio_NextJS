13b986a1c6c8816082bcfb7948b528723dfde010
AnthonyGKruger <60028700+AnthonyGKruger@users.noreply.github.com>
2023-05-14 18:52:28 +0200

    vercel analytics added.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,28 +3,29 @@ import Header from "@/components/UI/Header";
 import { useRouter } from "next/router";
 import Footer from "@/components/UI/Footer";
 import Banner from "@/components/UI/Banner";
-import { Provider } from 'react-redux';
-import store from '@/store/index';
+import { Provider } from "react-redux";
+import store from "@/store/index";
 import Companies from "@/components/UI/Companies";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-	
 	const { asPath } = useRouter();
 
 	return (
 		<Provider store={store}>
 			<Header />
 			<Banner
-					bgColor="bg-red"
-					content="This website is currently being developed, some functionality may
+				bgColor="bg-red"
+				content="This website is currently being developed, some functionality may
 					not work as expected!"
-					textColor="white"
-				/>
+				textColor="white"
+			/>
 			<main className={asPath !== "/" ? "vh-auto mb5" : ""}>
 				<Component {...pageProps} />
-				<Companies/>
+				<Analytics />
+				<Companies />
 			</main>
-			<Footer/>
+			<Footer />
 		</Provider>
 	);
 }
