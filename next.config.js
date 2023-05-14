7f6dd83d8db1237f667ba0963fc5cd9301378e2c
AnthonyGKruger <60028700+AnthonyGKruger@users.noreply.github.com>
2023-05-14 17:03:17 +0200

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-
-
-// module.exports = {
-//   webpack: (config, options) =>
-//   {
-//       config.module.rules.push({
-//           test: /\.pdf$/i,
-//           type: 'asset/source'
-//       })
-
-//       return config
-//   },
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
 // }
 
-module.exports = nextConfig
+
+
+module.exports = {
+  webpack: (config, options) =>
+  {
+      config.module.rules.push({
+          test: /\.pdf$/i,
+          type: 'asset/source'
+      })
+
+      return config
+  },
+}
+
+// module.exports = nextConfig
